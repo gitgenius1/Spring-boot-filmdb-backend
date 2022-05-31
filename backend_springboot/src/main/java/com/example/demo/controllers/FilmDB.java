f3694494a52b7acf6a258bfe65a703d34dc50635
System Administrator <root@MacBook-Pro-7.lan>
2022-05-31 15:18:39 +0100

    second commit

--- backend_springboot/src/main/java/com/example/demo/controllers/FilmDB.java
+++ backend_springboot/src/main/java/com/example/demo/controllers/FilmDB.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.models.Film;
 import com.example.demo.models.FilmDAO;
